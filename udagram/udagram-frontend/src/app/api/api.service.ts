eca7b249a292b5d190fbdd23ceb6b84678d658a2
AhmadEz <ahmadez@live.com>
2023-03-11 00:59:59 +0200

    edit package.json script from lint to lint --fix

--- udagram/udagram-frontend/src/app/api/api.service.ts
+++ udagram/udagram-frontend/src/app/api/api.service.ts
@@ -34,7 +34,7 @@ export class ApiService {
 
   get(endpoint): Promise<any> {
     console.log(this.httpOptions);
-    
+
     const url = `${API_HOST}${endpoint}`;
     const req = this.http.get(url, this.httpOptions).pipe(map(ApiService.extractData));
 
